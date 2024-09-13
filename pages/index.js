c1b996630ec07e148d1bafef70aec6173d125e1d
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2024-09-13 23:52:59 +0530

    Create main page to manage authentication and todos

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Layout from "../components/Layout";
+import TodoForm from "../components/TodoForm";
+import TodoList from "../components/TodoList";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+import Login from "../components/Login";
+import SignUp from "../components/SignUp";
+
+const Home = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Layout>
+      {user ? (
+        <>
+          <TodoForm />
+          <TodoList />
+        </>
+      ) : (
+        <div className="flex flex-col items-center">
+          <SignUp />
+          <Login />
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export default Home;
