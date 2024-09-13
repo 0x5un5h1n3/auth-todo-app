0d50d77d79e615b6b5996b11fd9a6f27c60d22d5
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2024-09-13 23:51:00 +0530

    Create Layout component to wrap application context

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import { AuthProvider } from "../context/AuthContext";
+import { TodoProvider } from "../context/TodoContext";
+
+const Layout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <TodoProvider>
+        <div className="max-w-4xl mx-auto p-4">{children}</div>
+      </TodoProvider>
+    </AuthProvider>
+  );
+};
+
+export default Layout;
